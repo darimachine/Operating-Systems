b4aa5459392f2882597533845632284b89b5680a
Serhan Chavdarliev <serhi1334@gmail.com>
2025-03-01 17:39:49 +0200

    added 2 more tasks

--- Shell/Commands/02-b-4000.sh
+++ Shell/Commands/02-b-4000.sh
@@ -1,1 +1,9 @@
-Копирайте най-малкия файл от тези, намиращи се в /etc, в home директорията си.
+#Копирайте най-малкия файл от тези, намиращи се в /etc, в home директорията си.
+# Variant 1
+find /etc -type f -exec wc -c {} \; 2> null | sort -nbr | tail -1 | cut -d " " -f 2 | xargs -I {} cp {} ~/
+# ИЛИ
+cp $(find /etc/ -type f -exec wc -c {} \; 2>null | sort -bnr | tail -1 | cut -d " " -f2) ~/
+#Variant 2
+find /etc -type f -printf "%s %p\n" 2> null | sort -bnr | tail -1 | cut -d " " -f2 | xargs -I {} cp {} ~
+# ИЛИ
+cp $(find /etc/ -type f -printf "%s %p\n" 2> null | sort -nbr | tail -1 | cut -d " " -f2) ~
