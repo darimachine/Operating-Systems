82446e62086ff74a3f2b68337897344d042e38ae
Serhan Chavdarliev <serhi1334@gmail.com>
2025-03-01 16:37:45 +0200

    added this task with description

--- Shell/Commands/02-a-9000.sh
+++ Shell/Commands/02-a-9000.sh
@@ -1,6 +1,25 @@
-от предната задача: когато вече сте получили myetc с файлове, архивирайте
-всички от тях, които започват с 'c' в архив, който се казва c_start.tar
+# от предната задача: когато вече сте получили myetc с файлове, архивирайте
+# всички от тях, които започват с 'c' в архив, който се казва c_start.tar
 
-изтрийте директорията myetc и цялото и съдържание
+# изтрийте директорията myetc и цялото и съдържание
 
-изтрийте архива c_start.tar
+# изтрийте архива c_start.tar
+
+#KOPIRAME PURVITE 10 REDA SHTOTO POSLE GURMI ZA PAMET
+find /etc -perm -444 | head | xargs -I {} cp -r {} ~/myetc
+##VARIAN 1-------------------------------------------------------------
+find myetc/ -name "c*" | xargs -I {} tar -rf c_start.tar {}
+#VARIANT 2-------------------------------------------------------
+find myetc/ -name "c*" -exec tar -rf c_start2.tar {} +
+#VARIANT 3--------------------------------------------------
+tar -cf c_start.tar $(find myetc/ -name "c*")
+
+
+#list archive
+tar -tf c_start.tar
+
+#delete
+rm -rf myetc
+
+#delete tar
+rm -i c_start.tar
