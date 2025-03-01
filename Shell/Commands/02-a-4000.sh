31b16c94d6ba03cb5e25b28e1b561360db829b6c
Serhan Chavdarliev <serhi1334@gmail.com>
2025-03-01 17:47:19 +0200

    added explanation

--- Shell/Commands/02-a-4000.sh
+++ Shell/Commands/02-a-4000.sh
@@ -1,4 +1,4 @@
-Създайте файл permissions.txt в home директорията си. За него дайте единствено - read права на потребителя създал файла, write and exec на групата, read and exec на всички останали. Направете го и с битове, и чрез "буквички".
+# Създайте файл permissions.txt в home директорията си. За него дайте единствено - read права на потребителя създал файла, write and exec на групата, read and exec на всички останали. Направете го и с битове, и чрез "буквички".
 
 touch ~/permissions.txt
 chmod u=r,g=wx,o=rx ~/permissions.txt
